6e349a487b3e1a923800ac604008189d00ed598c
dbabanne46-code <dbabanne46@gmail.com>
2026-03-14 16:47:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*" } });
 
 let players = {};
+let scores = { RED: 0, BLUE: 0 };
 
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
         players[socket.id] = {
             id: socket.id,
-            name: data.name || "Oyuncu",
-            team: data.team || "A",
+            name: data.name.substring(0, 12),
+            team: data.team,
             pos: { x: 0, y: 1.7, z: 0 },
             rot: { y: 0, p: 0 },
             health: 100
         };
-        socket.emit('init', { id: socket.id, players });
+        socket.emit('init', { id: socket.id, players, scores });
         socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
     });
 
@@ -29,11 +30,20 @@ io.on('connection', (socket) => {
 
     socket.on('hit', (data) => {
         const targetId = data.targetId;
-        if (players[targetId] && players[targetId].team !== players[socket.id].team) {
-            players[targetId].health -= 25;
+        const shooter = players[socket.id];
+        if (players[targetId] && shooter && players[targetId].team !== shooter.team) {
+            players[targetId].health -= 34; // 3 mermide öldürür
             if (players[targetId].health <= 0) {
                 players[targetId].health = 100;
-                io.emit('playerDead', { victim: targetId, killer: socket.id, victimTeam: players[targetId].team });
+                scores[shooter.team]++;
+                io.emit('playerDead', { 
+                    victim: targetId, 
+                    killer: socket.id, 
+                    victimName: players[targetId].name,
+                    killerName: shooter.name,
+                    victimTeam: players[targetId].team,
+                    scores: scores 
+                });
             } else {
                 io.emit('playerHit', { id: targetId, health: players[targetId].health });
             }
@@ -46,4 +56,5 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(process.env.PORT || 3000, () => { console.log("Sunucu Aktif!"); });
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
